b779cd76f9b3d5e0418ba5f88bc22d382c0b9661
Ada Taylor <ada@idu.com>
2020-07-26 16:54:04 -0400

    Generating code for pickling objs, and searching for paths
    Former-commit-id: 2071bf475d6fd7e36299544cb045d17c081567e6

--- table_path_code.py
+++ table_path_code.py
@@ -19,19 +19,20 @@ print(sys.path)
 
 import state_lattice_planner as slp
 import model_predictive_trajectory_generator as mptj
+from collections import defaultdict
 
 OPTION_SHOW_VISIBILITY = True
 OPTION_FORCE_GENERATE_VISIBILITY = False
 OPTION_FORCE_GENERATE_OBSTACLE_MAP = False
-OPTION_EXPORT = False
+OPTION_EXPORT = True
 
 # window dimensions
 length = 450
 width = 600
 pixels_to_foot = 20
 
-resolution_visibility = 5
-resolution_planning = 10
+resolution_visibility = 1
+resolution_planning = 30
 
 # divide by two because these are radiuses
 DIM_TABLE_RADIUS = int(4 * pixels_to_foot / 2.0)
@@ -54,9 +55,9 @@ COLOR_PERIPHERAL = (178, 221, 235) 	# light yellow
 COLOR_GOAL = (50, 168, 82) 			# green
 COLOR_START = (255, 255, 255) 		# white
 
-OBSTACLE_CLEAR = (0, 0, 0)
-OBSTACLE_BUFFER = (100, 100, 100)
-OBSTACLE_FULL = (255, 255, 255)
+COLOR_OBSTACLE_CLEAR = (0, 0, 0)
+COLOR_OBSTACLE_BUFFER = (100, 100, 100)
+COLOR_OBSTACLE_FULL = (255, 255, 255)
 
 
 goals = []
@@ -80,8 +81,228 @@ SCENARIO_IDENTIFIER = "new_scenario"
 
 FILENAME_PICKLE_VIS = 'generated/pickled_visibility'
 FILENAME_PICKLE_OBSTACLES = 'generated/pickled_obstacles'
-FILENAME_VIS_PREFIX = "generated/fig_vis_"
+FILENAME_VIS_PREFIX = "generated/fine_fig_vis_"
 FILENAME_OBSTACLE_PREFIX = "generated/fig_obstacles"
+
+# visibility = np.zeros((r_width, r_length))
+# for x in range(r_width):
+# 	for y in range(r_length):
+# 		rx = x*resolution_visibility
+# 		ry = y*resolution_visibility
+# 		score = 0
+# 		for obs in observers:
+# 			score += obs.get_visibility((rx,ry))
+
+# 		visibility[x,y] = score
+
+# visibility = visibility.T
+
+# nodes = width x length divided up by planning resolution\
+n_width = int(width / resolution_planning) + 1
+n_length = int(length / resolution_planning) + 1
+
+def dist(p0, p1):
+	return math.sqrt((p0[0] - p1[0])**2 + (p0[1] - p1[1])**2)
+
+def bresenham_line(xy1, xy2):
+	x0, y0 = xy1
+	x1, y1 = xy2
+
+	steep = abs(y1 - y0) > abs(x1 - x0)
+	if steep:
+		x0, y0 = y0, x0  
+		x1, y1 = y1, x1
+
+	switched = False
+	if x0 > x1:
+		switched = True
+		x0, x1 = x1, x0
+		y0, y1 = y1, y0
+
+	if y0 < y1: 
+		ystep = 1
+	else:
+		ystep = -1
+
+	deltax = x1 - x0
+	deltay = abs(y1 - y0)
+	error = -deltax / 2
+	y = y0
+
+	line = []	
+	for x in range(x0, x1 + 1):
+		if steep:
+			line.append((y,x))
+		else:
+			line.append((x,y))
+
+		error = error + deltay
+		if error > 0:
+			y = y + ystep
+			error = error - deltax
+	if switched:
+		line.reverse()
+	return line
+
+def in_bounds(point):
+	x, y = point
+
+	if x > width or x < 0:
+		return False
+
+	if y > length or y < 0:
+		return False
+
+	return True
+
+
+def get_cost_of_move(pos1, pos2, obstacle_map, visibility_map):
+	cost = 0
+	most_vis = np.amax(visibility_map)
+	obstacle_max = np.amax(obstacle_map)
+	obstacle_min = np.amin(obstacle_map)
+
+	# print(obstacle_map[0][0])
+
+	# print(most_vis)
+	# print(visibility_map[0][0])
+	# print(obstacle_min)
+	# print(obstacle_max)
+	# exit()
+
+	line = bresenham_line(pos1, pos2)
+	# print(line) 
+
+	for point in line:
+		# infinite cost to go through obstacles
+		min_val = 1
+		if in_bounds(point):
+			# cost += 1 + (obstacle_map[point] * 500 * 0)
+			vis_x = int(point[0] / resolution_visibility)
+			vis_y = int(point[1] / resolution_visibility)
+			# print(visibility_map.shape)
+			# print(vis_x, vis_y)
+			vis = visibility_map[vis_y][vis_x]
+			cost_increase = 1
+			# cost_increase = ((most_vis - vis) / most_vis)
+			cost += cost_increase
+			
+			if cost_increase < min_val:
+				min_val = cost_increase
+
+
+			# print((vis_x, vis_y, vis, obstacle_map[point]))
+
+			# print("point info")
+			# print((vis, obstacle_map[point], cost))
+
+			if math.isnan(cost):
+				exit()
+
+		else:
+			cost += np.Inf
+
+			# Give a bonus for being at least partially well observed in this segment
+	# cost += len(line)*min_val
+
+	return cost
+
+
+def get_children_astar(node, obstacle_map, visibility_map):
+	xyz, parent_node, cost_so_far = node
+	children = []
+	directions = [(0, resolution_planning), (resolution_planning, 0), (0, -1*resolution_planning), (-1*resolution_planning, 0)]
+
+	for direction in directions:
+		new_xyz = tuple_plus(xyz, direction)
+		cost_to_add = get_cost_of_move(xyz, new_xyz, obstacle_map, visibility_map)
+
+		# print(cost_to_add)
+
+		new_node = (new_xyz, node, cost_so_far + cost_to_add)
+		children.append(new_node)
+
+
+	return children
+
+
+def heuristic(node, goal, visibility_map):
+	pos1 = node[0]
+	pos2 = goal
+
+	most_vis = np.amax(visibility_map)
+	ratio = (1 / most_vis)
+	len(bresenham_line(pos1, pos2)) * ratio
+
+	return len(bresenham_line(pos1, pos2))
+
+def get_paths_astar(start, goal, obstacle_map, visibility_maps):
+	paths = []
+
+	obstacle_map = obstacle_map
+	vis_maps = []
+	vis_maps.append(visibility_maps[VIS_ALL][0])
+	vis_maps.append(visibility_maps[VIS_TABLE][0])
+	vis_maps.append(visibility_maps[VIS_INDIVIDUALS][0])
+	# vis_maps.append(visibility_maps[VIS_INDIVIDUALS][1])
+
+	print(len(vis_maps))
+	
+	for visibility_map in vis_maps:
+		print("math path being added")
+
+		# node = ((x,y), parent_node, cost-so-far)
+
+		n_0 = (start, None, 0)
+
+		parent_grid = {}
+
+		openset = [n_0]
+		closed_set = defaultdict(lambda: float('inf'))
+
+		i = 0
+		goal_found = False
+		while i < 600000 and not goal_found:
+			i += 1
+			openset = sorted(openset, key=lambda x: x[2] + heuristic(x, goal, visibility_map))
+			# print(openset)
+			# Get best bet node
+			node = openset.pop(0)
+			xyz, parent, cost = node
+			closed_set[xyz] = (cost, parent)
+
+			# print(len(closed_set))
+			
+			kids = get_children_astar(node, obstacle_map, visibility_map)
+			openset.extend(kids)
+			# print(len(openset))
+
+			print(dist(xyz, goal))
+
+			if dist(xyz, goal) < resolution_planning:
+				print("Found the goal!")
+				goal_found = True
+
+				path = []
+				n_path = node
+				while n_path[1] is not None:
+					n_path = n_path[1]
+					path.append(n_path[0])
+				
+				print(path)
+				paths.append(path[::-1])
+		
+	return paths
+
+
+
+
+
+	# assemble the completed path
+
+
+
+
 
 
 def get_path(start, end, obs=[]):
@@ -113,7 +334,6 @@ def get_path_2(start, end, obs=[]):
 	print(yaw)
 	print(p)
 	return path
-
 
 
 def tuple_plus(a, b):
@@ -207,7 +427,7 @@ class Table:
 class Observer:
 	entity_radius = DIM_OBSERVER_RADIUS
 	draw_depth = 50
-	cone_depth = max(length, width)*.5
+	cone_depth = max(length, width)*2
 	focus_angle = 60 / 2.0
 	peripheral_angle = 120 / 2.0
 
@@ -356,7 +576,7 @@ if generate_type == TYPE_PLOTTED:
 
 elif generate_type == TYPE_RANDOM:
 	random_id = ''.join([random.choice(string.ascii_letters 
-            + string.digits) for n in range(10)]) 
+			+ string.digits) for n in range(10)]) 
 	SCENARIO_IDENTIFIER = "new_scenario_" + random_id
 
 	start = get_random_point_in_room(length, width)
@@ -384,9 +604,11 @@ elif generate_type == TYPE_RANDOM:
 else:
 	print("Incorrect generate_type")
 
+print(goals)
+
 # Get paths
 # goal = random.choice(goals)
-goal = goals[5]
+goal = goals[4]
 path = get_path(start, goal)
 
 obstacle_vis = np.zeros((length,width,3), np.uint8)
@@ -399,8 +621,8 @@ img = np.zeros((length,width,3), np.uint8)
 for table in tables:
 	cv2.rectangle(img, table.pt_top_left(), table.pt_bottom_right(), COLOR_TABLE, table.get_radius())
 
-	cv2.rectangle(obstacle_vis, table.pt_top_left(), table.pt_bottom_right(), OBSTACLE_BUFFER, table.get_radius() + DIM_NAVIGATION_BUFFER)
-	cv2.rectangle(obstacle_vis, table.pt_top_left(), table.pt_bottom_right(), OBSTACLE_FULL, table.get_radius())
+	cv2.rectangle(obstacle_vis, table.pt_top_left(), table.pt_bottom_right(), COLOR_OBSTACLE_BUFFER, table.get_radius() + DIM_NAVIGATION_BUFFER)
+	cv2.rectangle(obstacle_vis, table.pt_top_left(), table.pt_bottom_right(), COLOR_OBSTACLE_FULL, table.get_radius())
 
 	# cv2.rectangle(obstacle_map, table.pt_top_left(), table.pt_bottom_right(), 1, table.get_radius() + DIM_NAVIGATION_BUFFER)
 	# cv2.rectangle(obstacle_map, table.pt_top_left(), table.pt_bottom_right(), 1, table.get_radius())
@@ -411,8 +633,8 @@ for obs in observers:
 	# Draw person
 	cv2.circle(img, obs.get_center(), obs.get_radius(), COLOR_OBSERVER, obs.get_radius())
 
-	cv2.circle(obstacle_vis, obs.get_center(), obs.get_radius(), OBSTACLE_BUFFER, obs.get_radius() + DIM_NAVIGATION_BUFFER)
-	cv2.circle(obstacle_vis, obs.get_center(), obs.get_radius(), OBSTACLE_FULL, obs.get_radius())
+	cv2.circle(obstacle_vis, obs.get_center(), obs.get_radius(), COLOR_OBSTACLE_BUFFER, obs.get_radius() + DIM_NAVIGATION_BUFFER)
+	cv2.circle(obstacle_vis, obs.get_center(), obs.get_radius(), COLOR_OBSTACLE_FULL, obs.get_radius())
 
 	# cv2.circle(obstacle_map, obs.get_center(), obs.get_radius(), 1, obs.get_radius() + DIM_NAVIGATION_BUFFER)
 	# cv2.circle(obstacle_map, obs.get_center(), obs.get_radius(), 1, obs.get_radius())
@@ -451,14 +673,6 @@ obstacles = pickle.load(dbfile)
 obstacle_map = obstacles['map']
 obstacle_vis = obstacles['vis']
 dbfile.close() 
-
-# Draw the path
-for i in range(len(path) - 1):
-	a = path[i]
-	b = path[i + 1]
-
-	cv2.line(img, a, b, COLOR_START, thickness=2, lineType=8)
-
 
 # VISIBILITY Unit TEST
 if False:
@@ -563,6 +777,55 @@ visibility_maps = pickle.load(dbfile)
 resolution_visibility = visibility_maps[VIS_INFO_RESOLUTION]
 print("Found maps at resolution " + str(resolution_visibility))
 dbfile.close() 
+
+# more vis
+p1 = [(490, 270), (490, 240), (490, 210), (460, 210), (430, 210), (400, 210), (370, 210), (340, 210), (310, 210), (280, 210), (250, 210), (220, 210), (190, 210), (160, 210), (130, 210), (100, 210), (70, 210)]
+# less vis
+p2 = [(490, 270),(490, 240),(460, 240),(460, 210),(430, 210),(400, 210),(370, 210),(340, 210),(310, 210),(280, 210),(250, 210),(220, 210),(190, 210),(160, 210),(130, 210),(100, 210),(70, 210)]
+
+p_names = ["first", "second"]
+
+paths = [p1, p2]
+for visibility_map in visibility_maps:
+	print("a map")
+	for path in paths:
+		print("this path")
+		cost = 0
+		for i in range(len(path) - 1):
+			pos1 = path[i]
+			pos2 = path[i + 1]
+			cost += get_cost_of_move(pos1, pos2, obstacle_map, visibility_map)
+
+		print("Cost = " + str(cost))
+
+
+
+# paths = get_paths_astar(start, goal, obstacle_map, visibility_maps)
+#p = [(490, 270), (490, 240), (490, 210), (460, 210), (430, 210), (400, 210), (370, 210), (340, 210), (310, 210), (280, 210), (250, 210), (220, 210), (190, 210), (160, 210), (130, 210), (100, 210), (70, 210)]
+# p = [(490, 270),(490, 240),(460, 240),(460, 210),(430, 210),(400, 210),(370, 210),(340, 210),(310, 210),(280, 210),(250, 210),(220, 210),(190, 210),(160, 210),(130, 210),(100, 210),(70, 210)]
+
+paths = [p[::-1]]
+
+path_colors = ((0,255,255), (138,43,226), (255,64,64), (0,201,87))
+path_titles = ["ALL", "TABLE", "INDIVIDUAL"]
+
+all_paths_img = img.copy()
+
+for pi in range(len(paths)):
+	path = paths[pi]
+	path_img = img.copy()
+	path_title = path_titles[pi]
+
+	# Draw the path  
+	for i in range(len(path) - 1):
+		a = path[i]
+		b = path[i + 1]
+		
+		cv2.line(path_img, a, b, path_colors[pi], thickness=2, lineType=8)
+		cv2.line(all_paths_img, a, b, path_colors[pi], thickness=2, lineType=8)
+	cv2.imwrite('generated/fig_path_' + path_title + '.png', path_img) 
+
+cv2.imwrite('generated/fig_path_' + "VARIETY" + '.png', all_paths_img) 
 
 
 if OPTION_EXPORT:
