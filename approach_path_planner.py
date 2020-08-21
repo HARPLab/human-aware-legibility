c8e9cc262b61217b1666481473897e3a530fccd1
Ada Taylor <ada@idu.com>
2020-08-21 16:31:03 -0400

    outline for path creation and assessment
    Former-commit-id: dfb1bccfa04141f295128f6f565d7c9d225235c9

--- approach_path_planner.py
+++ approach_path_planner.py
@@ -1,3 +1,34 @@
 import table_path_code
 
+# start 		= r.get_start()
+# goals 		= r.get_goals_all()
+# goal 		= r.get_current_goal()
+# observers 	= r.get_observers()
+# tables 		= r.get_tables()
+# waypoints 	= r.get_waypoints()
+# SCENARIO_IDENTIFIER = r.get_scenario_identifier()
+
+VISIBILITY_TYPES = VIS_CHECKLIST
+
+def generate_paths(num_paths, r):
+	pass
+
+
+def assess_paths():
+	pass
+
+
+NUM_PATHS = 200
+
+generate_type = TYPE_UNITY_ALIGNED
+r 	= Restaurant(generate_type)
+vis = r.get_visibilities()
+v 	= r.get_visibility_maps()
+
+path_options 		= generate_paths()
+path_assessments 	= assess_paths()
+
+draw_paths(r, paths_dict)
+export(r, paths_dict)
+
 print("Done")
