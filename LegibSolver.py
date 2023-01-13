ba585456f069255e3adf4ae6b60ca9bff8841fdd
Ada Taylor <ada@idu.com>
2023-01-13 17:45:01 -0500

    Moved solver to its own file for easier modularity

---
+++ LegibSolver.py
@@ -0,0 +1,88 @@
+import copy
+import time
+import numpy as np
+import matplotlib.pyplot as plt
+
+from ilqr import iLQR
+from ilqr.cost import QRCost, PathQRCost
+from ilqr.dynamics import constrain
+from ilqr.dynamics import tensor_constrain
+
+from NavigationDynamics import NavigationDynamics
+
+J_hist = []
+def on_iteration(iteration_count, xs, us, J_opt, accepted, converged):
+    J_hist.append(J_opt)
+    info = "converged" if converged else ("accepted" if accepted else "failed")
+
+    final_state = xs[-1]
+    print("iteration", iteration_count, info, J_opt, final_state)
+
+
+def run_solver(exp):
+    exp.reinit_file_id()
+
+    x0_raw          = exp.get_start()    # initial state
+    x_goal_raw      = exp.get_target_goal()
+
+    # dynamics = AutoDiffDynamics(f, [x], [u], t)
+    dynamics = NavigationDynamics(exp.get_dt())
+
+    # Note that the augmented state is not all 0.
+    x0      = dynamics.augment_state(np.array(x0_raw)).T
+    x_goal  = dynamics.augment_state(np.array(x_goal_raw)).T
+
+    N       = exp.get_N()
+    dt      = exp.get_dt()
+
+    x_T     = N
+    Xrefline = np.tile(x_goal_raw, (N+1, 1))
+    Xrefline = np.reshape(Xrefline, (-1, 2))
+
+    u_blank = np.asarray([0.0, 0.0])
+    Urefline = np.tile(u_blank, (N, 1))
+    Urefline = np.reshape(Urefline, (-1, 2))
+
+    state_size  = 2 #
+    action_size = 2 # 
+
+    ### EXP IS USED AFTER THIS POINT
+    cost = exp.setup_cost(Xrefline, Urefline)
+
+    # l = leg_cost.l
+    # l_terminal = leg_cost.term_cost
+    # cost = AutoDiffCost(l, l_terminal, x_inputs, u_inputs)
+
+    FLAG_JUST_PATH = False
+    if FLAG_JUST_PATH:
+        traj        = Xrefline
+        us_init     = Urefline
+        cost        = PathQRCost(Q, R, traj, us_init)
+        print("Set to old school pathing")
+        exit()
+
+    # default value from text
+    max_reg = 1e-10 #None # default value is 1e-10
+    ilqr = iLQR(dynamics, cost, N, max_reg=None)
+
+    tol = 1e-6
+    # tol = 1e-10
+
+    num_iterations = 100
+
+    start_time = time.time()
+    xs, us = ilqr.fit(x0_raw, Urefline, tol=tol, n_iterations=num_iterations, on_iteration=on_iteration)
+    end_time = time.time()
+
+    t = np.arange(N) * dt
+    theta = np.unwrap(xs[:, 0])  # Makes for smoother plots.
+    theta_dot = xs[:, 1]
+
+    # Plot of the path through space
+    verts = xs
+    xs, ys = zip(*verts)
+    gx, gy = zip(*exp.get_goals())
+    sx, sy = zip(*[x0_raw])
+
+    elapsed_time = end_time - start_time
+    cost.graph_legibility_over_time(verts, us, elapsed_time=elapsed_time)
--- iLQR_for_legibility.py
+++ iLQR_for_legibility.py
@@ -31,21 +31,13 @@ from OALegiblePathQRCost import OALegiblePathQRCost
 from NavigationDynamics import NavigationDynamics
 import LegibTestScenarios as test_scenarios
 import LegibTestExperiments as test_exp
+import LegibSolver as solver
 
 from sklearn.preprocessing import normalize
 
 import utility_environ_descrip as resto
 
-J_hist = []
 dynamics = None
-
-def on_iteration(iteration_count, xs, us, J_opt, accepted, converged):
-    J_hist.append(J_opt)
-    info = "converged" if converged else ("accepted" if accepted else "failed")
-
-    final_state = xs[-1]
-    print("iteration", iteration_count, info, J_opt, final_state)
-
 
 def get_window_dimensions_for_envir(self, start, goals, pts):
     xmin, xmax, ymin, ymax = 0.0, 0.0, 0.0, 0.0
@@ -73,75 +65,6 @@ def get_window_dimensions_for_envir(self, start, goals, pts):
 
     return xmin - xbuffer, xmax + xbuffer, ymin - ybuffer, ymax + ybuffer
 
-def run_solver(exp):
-    exp.reinit_file_id()
-
-    x0_raw          = exp.get_start()    # initial state
-    x_goal_raw      = exp.get_target_goal()
-
-    # dynamics = AutoDiffDynamics(f, [x], [u], t)
-    dynamics = NavigationDynamics(exp.get_dt())
-
-    # Note that the augmented state is not all 0.
-    x0      = dynamics.augment_state(np.array(x0_raw)).T
-    x_goal  = dynamics.augment_state(np.array(x_goal_raw)).T
-
-    N       = exp.get_N()
-    dt      = exp.get_dt()
-
-    x_T     = N
-    Xrefline = np.tile(x_goal_raw, (N+1, 1))
-    Xrefline = np.reshape(Xrefline, (-1, 2))
-
-    u_blank = np.asarray([0.0, 0.0])
-    Urefline = np.tile(u_blank, (N, 1))
-    Urefline = np.reshape(Urefline, (-1, 2))
-
-    state_size  = 2 #
-    action_size = 2 # 
-
-    ### EXP IS USED AFTER THIS POINT
-    cost = exp.setup_cost(Xrefline, Urefline)
-
-    # l = leg_cost.l
-    # l_terminal = leg_cost.term_cost
-    # cost = AutoDiffCost(l, l_terminal, x_inputs, u_inputs)
-
-    FLAG_JUST_PATH = False
-    if FLAG_JUST_PATH:
-        traj        = Xrefline
-        us_init     = Urefline
-        cost        = PathQRCost(Q, R, traj, us_init)
-        print("Set to old school pathing")
-        exit()
-
-    # default value from text
-    max_reg = 1e-10 #None # default value is 1e-10
-    ilqr = iLQR(dynamics, cost, N, max_reg=None)
-
-    tol = 1e-6
-    # tol = 1e-10
-
-    num_iterations = 100
-
-    start_time = time.time()
-    xs, us = ilqr.fit(x0_raw, Urefline, tol=tol, n_iterations=num_iterations, on_iteration=on_iteration)
-    end_time = time.time()
-
-    t = np.arange(N) * dt
-    theta = np.unwrap(xs[:, 0])  # Makes for smoother plots.
-    theta_dot = xs[:, 1]
-
-    # Plot of the path through space
-    verts = xs
-    xs, ys = zip(*verts)
-    gx, gy = zip(*exp.get_goals())
-    sx, sy = zip(*[x0_raw])
-
-    elapsed_time = end_time - start_time
-    cost.graph_legibility_over_time(verts, us, elapsed_time=elapsed_time)
-
-
 def exp_plain_symmetrical(goal_index=None, obs_angle=0):
     ####################################
     ### SET UP EXPERIMENT
@@ -167,7 +90,7 @@ def exp_plain_symmetrical(goal_index=None, obs_angle=0):
     # exp.set_f_label(ex.F_VIS_LIN)
     exp.set_f_label(ex.F_VIS_BIN)
 
-    run_solver(exp)
+    solver.run_solver(exp)
 
 def exp_observers(goal_index=None, obs_angle=None):
     ####################################
@@ -198,7 +121,7 @@ def exp_observers(goal_index=None, obs_angle=None):
     exp.set_f_label(ex.F_VIS_BIN)
     # exp.set_f_label(ex.F_NONE)
 
-    run_solver(exp)
+    solver.run_solver(exp)
 
 def exp_obstacles(goal_index=None):
     ####################################
@@ -227,7 +150,7 @@ def exp_obstacles(goal_index=None):
     exp.set_solver_scale_obstacle(1000.0)
     exp.set_solver_scale_term(1000.0)
 
-    run_solver(exp)
+    solver.run_solver(exp)
 
     ###### RUN ADDITIONAL REMIXES
     # exp.set_f_label(ex.F_NONE)
@@ -265,7 +188,7 @@ def exp_obstacles_and_observers(goal_index=None, obs_angle=0):
  
     exp.set_N(30)
 
-    run_solver(exp)
+    solver.run_solver(exp)
 
     ###### RUN ADDITIONAL REMIXES
     # exp.set_f_label(ex.F_NONE)
