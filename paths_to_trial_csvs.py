cb8e29f34f74477ccf6a0578b520094da7fafaf1
Ada Taylor <ada@idu.com>
2024-04-04 15:07:28 -0400

    Helper functions for turning the exported paths from the solver into paths ready for deployment

---
+++ paths_to_trial_csvs.py
@@ -0,0 +1,30 @@
+### ABC
+### DEF
+
+#ab = cb horizontal flip
+#cd = fd horizontal flip
+# 
+# 
+
+
+# GENERATED PATHS
+ab = 
+ac = 
+ad = 
+ae = 
+af = 
+
+ba = 
+bc = 
+bd = 
+be = 
+bf = 
+
+
+
+
+
+
+
+
+
