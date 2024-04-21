143906cb8e95f4e38bb4b81f0272a9cb1394c9c6
Ada Taylor <ada@idu.com>
2024-04-21 14:47:32 -0400

    Import both types of data and stub out

--- analysis/analysis_summer_2024.py
+++ analysis/analysis_summer_2024.py
@@ -1,8 +1,6 @@
 import os
 import pandas as pd
-
-keypress_path = "keypress_log.csv"
-keypress_data = []
+import copy
 
 ##### Import keypress file data
 ##### Next: shillelagh to import directly from file?
@@ -15,31 +13,72 @@ keypress_data = []
 #     keypress_data.append(clean_data)
 # keypress_file.close()
 
+keypress_path           = "keypress_log.csv"
+FLAG_ANALYZE_EXPANDED   = False
+
+keypress_data = []
 df_keypress = pd.read_csv(keypress_path)
 
 ##### Import the robot movement data
 dir_list = os.listdir("mission_reports/")
 dir_list = [f for f in dir_list if 'csv' in f]
 
+mini_list = [f for f in dir_list if 'mini' in f]
+mission_list = [f for f in dir_list if 'mission' in f]
 
-df_chunks = []
+print("IMPORTING")
+print(mini_list)
+print(mission_list)
+
+df_chunks_mini      = []
+df_chunks_mission   = []
 
 trial_names = []
 ### SIMPLIFIED DATA
-for file_name in dir_list:
+for file_name in mini_list:
     waypoints_path = "mission_reports/" + file_name
     path_name = file_name.replace('.csv','')
 
-    keypress_file   = open(waypoints_path)
-    df_chunk        = pd.read_csv(waypoints_path, columns=['path_label', 'status', 'timestamp', 'time_elapsed'])
+    keypress_file   = open(waypoints_path)    
+    df_chunk        = pd.read_csv(waypoints_path) #, columns=['path_label', 'status', 'timestamp', 'time_elapsed'])
 
     ### Label with the matching mission file
     ### If needed, also put participant number on here
     df_chunk['trial_group'] = path_name
     
     trial_names.append(path_name)
-    df_chunks.append(copy.copy(df_chunk))
+    df_chunks_mini.append(copy.copy(df_chunk))
 
+
+if FLAG_ANALYZE_EXPANDED:
+    ### FULL DATA
+    for file_name in mission_list:
+        waypoints_path = "mission_reports/" + file_name
+        path_name = file_name.replace('.csv','')
+
+        keypress_file   = open(waypoints_path)
+        ## TODO: clean it first of parking flags?
+        df_chunk        = pd.read_csv(waypoints_path) #, columns=['path_label', 'status', 'timestamp', 'time_elapsed'])
+
+        ### Label with the matching mission file
+        ### If needed, also put participant number on here
+        df_chunk['trial_group'] = path_name
+        
+        trial_names.append(path_name)
+        df_chunks_mission.append(copy.copy(df_chunk))
 
 ##### Turn them both into a single Pandas?
+
+# For each trial in the deck
+# match up the trial with button presses along the way
+# Also match up with locations in space via expanded set
+
+
+##### Run comparisons across symmetrical sets?
+
+
+
+
+
+
 
